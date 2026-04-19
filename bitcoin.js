488d312f7aea9dbc1eec0314c2a49eb31cd84300
Julian22077 <camilolozano0122@gmail.com>
2026-04-19 02:21:22 +0000

    ..

---
+++ bitcoin.js
@@ -0,0 +1,30 @@
+[
+    {
+    "id": "bitcoin",
+    "symbol": "btc",
+    "name": "Bitcoin",
+    "image": "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+    "current_price": 75658,
+    "market_cap": 1514499298248,
+    "market_cap_rank": 1,
+    "fully_diluted_valuation": 1514499298248,
+    "total_volume": 51414350613,
+    "high_24h": 77391,
+    "low_24h": 75541,
+    "price_change_24h": -1719.35685597992,
+    "price_change_percentage_24h": -2.22205,
+    "market_cap_change_24h": -32028166515.5149,
+    "market_cap_change_percentage_24h": -2.07097,
+    "circulating_supply": 20017787,
+    "total_supply": 20017787,
+    "max_supply": 21000000,
+    "ath": 126080,
+    "ath_change_percentage": -39.99227,
+    "ath_date": "2025-10-06T18:57:42.558Z",
+    "atl": 67.81,
+    "atl_change_percentage": 111474.68589,
+    "atl_date": "2013-07-06T00:00:00.000Z",
+    "roi": null,
+    "last_updated": "2026-04-19T02:18:02.115Z"
+  }
+]
